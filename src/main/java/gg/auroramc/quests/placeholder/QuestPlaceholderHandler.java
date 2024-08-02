f00bbd068e17632a3c1cee644882a6ec18df5604
Erik Szabó <erik1szabo1@gmail.com>
2024-08-02 17:46:46 +0200

    Add papi placeholders

--- src/main/java/gg/auroramc/quests/placeholder/QuestPlaceholderHandler.java
+++ src/main/java/gg/auroramc/quests/placeholder/QuestPlaceholderHandler.java
@@ -1,8 +1,12 @@
 package gg.auroramc.quests.placeholder;
 
+import gg.auroramc.aurora.api.AuroraAPI;
 import gg.auroramc.aurora.api.placeholder.PlaceholderHandler;
+import gg.auroramc.quests.AuroraQuests;
+import gg.auroramc.quests.util.RomanNumber;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class QuestPlaceholderHandler implements PlaceholderHandler {
@@ -12,12 +16,50 @@ public class QuestPlaceholderHandler implements PlaceholderHandler {
     }
 
     @Override
-    public String onPlaceholderRequest(Player player, String[] strings) {
+    public String onPlaceholderRequest(Player player, String[] args) {
+        if (args.length < 2) return null;
+        var manager = AuroraQuests.getInstance().getQuestManager();
+        var full = String.join("_", args);
+
+        if (full.endsWith("level_roman")) {
+            var pool = manager.getQuestPool(full.substring(0, full.length() - 12));
+            if (pool == null) return null;
+            return RomanNumber.toRoman(pool.getPlayerLevel(player));
+        } else if (full.endsWith("level_raw")) {
+            var pool = manager.getQuestPool(full.substring(0, full.length() - 10));
+            if (pool == null) return null;
+            return String.valueOf(pool.getPlayerLevel(player));
+        } else if (full.endsWith("level")) {
+            var pool = manager.getQuestPool(full.substring(0, full.length() - 7));
+            if (pool == null) return null;
+            return AuroraAPI.formatNumber(pool.getPlayerLevel(player));
+        } else if (full.endsWith("count_raw")) {
+            var pool = manager.getQuestPool(full.substring(0, full.length() - 10));
+            if (pool == null) return null;
+            return String.valueOf(pool.getCompletedQuestCount(player));
+        } else if (full.endsWith("count")) {
+            var pool = manager.getQuestPool(full.substring(0, full.length() - 7));
+            if (pool == null) return null;
+            return AuroraAPI.formatNumber(pool.getCompletedQuestCount(player));
+        }
+
         return null;
     }
 
     @Override
     public List<String> getPatterns() {
-        return List.of();
+        var manager = AuroraQuests.getInstance().getQuestManager();
+
+        var list = new ArrayList<String>();
+
+        for (var pool : manager.getQuestPools()) {
+            list.add(pool.getId() + "_level");
+            list.add(pool.getId() + "_level_roman%");
+            list.add(pool.getId() + "_level_raw");
+            list.add(pool.getId() + "_count");
+            list.add(pool.getId() + "_count_raw");
+        }
+
+        return list;
     }
 }
