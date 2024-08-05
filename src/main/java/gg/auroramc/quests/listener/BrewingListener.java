3b26ca63fbd91fb55c5ff85443a711e4e91f5fa9
Erik Szabó <erik1szabo1@gmail.com>
2024-08-05 13:09:24 +0200

    Fix BREW task type

--- src/main/java/gg/auroramc/quests/listener/BrewingListener.java
+++ src/main/java/gg/auroramc/quests/listener/BrewingListener.java
@@ -1,9 +1,11 @@
 package gg.auroramc.quests.listener;
 
+import gg.auroramc.aurora.api.util.BukkitPotionType;
+import gg.auroramc.aurora.api.util.Version;
 import gg.auroramc.quests.AuroraQuests;
 import gg.auroramc.quests.api.quest.TaskType;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,16 +17,14 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
-import org.bukkit.potion.PotionEffect;
+import org.bukkit.persistence.PersistentDataType;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 
 public class BrewingListener implements Listener {
-    private final HashMap<Location, UUID> brewingStands = new HashMap<>();
+    private NamespacedKey potionKey = new NamespacedKey(AuroraQuests.getInstance(), "counted");
+    private final Map<Location, Player> brewingStands = new HashMap<>();
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onInventoryOpen(InventoryOpenEvent event) {
@@ -38,39 +38,41 @@ public class BrewingListener implements Listener {
             return;
         }
 
-        brewingStands.put(inventory.getLocation(), event.getPlayer().getUniqueId());
+        if (event.getPlayer() instanceof Player player) {
+            brewingStands.put(inventory.getLocation(), player);
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onBrew(BrewEvent event) {
-        UUID uuid;
-        if ((uuid = brewingStands.get(event.getBlock().getLocation())) == null) {
-            return;
-        }
+        var player = brewingStands.remove(event.getBlock().getLocation());
 
-        Player player = Bukkit.getPlayer(uuid);
         if (player == null || player.hasMetadata("NPC")) {
             return;
         }
 
-
-        var potionKeys = new ArrayList<String>();
-
         for (ItemStack item : event.getResults()) {
-            if (item != null && item.hasItemMeta() && item.getItemMeta() instanceof PotionMeta) {
-                PotionMeta meta = (PotionMeta) item.getItemMeta();
-                for (PotionEffect effect : meta.getCustomEffects()) {
-                    var type = effect.getType();
-                    int level = effect.getAmplifier() + 1; // Amplifier starts at 0 for level I potions
-                    potionKeys.add(type + ";" + level);
-
+            if (item != null && item.hasItemMeta() && item.getItemMeta() instanceof PotionMeta meta) {
+                if (meta.getPersistentDataContainer().has(potionKey, PersistentDataType.BYTE)) {
+                    continue;
                 }
+
+                var type = new BukkitPotionType(meta);
+                var typeString = type.getType().name().toLowerCase(Locale.ROOT);
+
+                if (!Version.isAtLeastVersion(20, 2)) {
+                    if (type.isExtended()) {
+                        typeString = "long_" + typeString;
+                    } else if (type.isUpgraded()) {
+                        typeString = "strong_" + typeString;
+                    }
+                }
+
+                AuroraQuests.getInstance().getQuestManager().progress(player, TaskType.BREW, 1, Map.of("type", typeString));
+
+                meta.getPersistentDataContainer().set(potionKey, PersistentDataType.BYTE, (byte) 1);
+                item.setItemMeta(meta);
             }
-        }
-
-
-        for (var key : potionKeys) {
-            AuroraQuests.getInstance().getQuestManager().progress(player, TaskType.BREW, 1, Map.of("type", key));
         }
     }
 }
