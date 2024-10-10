a1b1172ea0d1e335e14fecf8cc6540bf5f39e5fb
Erik Szabó <erik1szabo1@gmail.com>
2024-10-10 23:21:13 +0200

    Add LevelledMobs support for KILL_LEVELLED_MOB task type

--- src/main/java/gg/auroramc/quests/listener/MobKillingListener.java
+++ src/main/java/gg/auroramc/quests/listener/MobKillingListener.java
@@ -3,12 +3,15 @@ package gg.auroramc.quests.listener;
 import gg.auroramc.aurora.api.AuroraAPI;
 import gg.auroramc.quests.AuroraQuests;
 import gg.auroramc.quests.api.quest.TaskType;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.Map;
 
@@ -31,5 +34,18 @@ public class MobKillingListener implements Listener {
             var typeId = AuroraAPI.getItemManager().resolveId(drop);
             manager.progress(player, TaskType.ENTITY_LOOT, drop.getAmount(), Map.of("type", typeId));
         }
+
+        var level = getMobLevel(entity);
+        if (level != null) {
+            manager.progress(player, TaskType.KILL_LEVELLED_MOB, 1, Map.of("type", id, "level", level));
+        }
+    }
+
+    private Double getMobLevel(LivingEntity livingEntity) {
+        var levelledMobsPlugin = Bukkit.getPluginManager().getPlugin("LevelledMobs");
+        if (levelledMobsPlugin == null) return null;
+        var levelKey = new NamespacedKey(levelledMobsPlugin, "level");
+        var level = livingEntity.getPersistentDataContainer().get(levelKey, PersistentDataType.INTEGER);
+        return level != null ? level.doubleValue() : null;
     }
 }
